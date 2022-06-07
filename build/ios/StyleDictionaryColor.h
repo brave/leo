b741012c85e6165ea4dd9ad3c352420ae0dceabe
Jacob Lamont <cob@jacoblamont.com>
2022-06-07 15:47:49 -0400

    initial commit

---
+++ build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+
+// Do not edit directly
+// Generated on Thu, 16 Dec 2021 18:06:29 GMT
+
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorColorsMultipleFills0,
+ColorColorsMultipleFills1,
+ColorColorsSingleBlue,
+ColorColorsRefBlue,
+ColorColorsSpecialCharacters,
+ColorColorsSpecialCharactersNderung,
+ColorLightBackground,
+ColorDarkBackground
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
