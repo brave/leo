f00dd5beb8bfba244fc6bf70d730da50eb53cb48
Jacob Lamont <cob@jacoblamont.com>
2022-08-18 17:42:26 -0400

    fixes tailwind build

--- tokens/transformation/tailwind/static/preflightPlugin.js
+++ tokens/transformation/tailwind/static/preflightPlugin.js
@@ -1,5 +1,5 @@
 const plugin = require("tailwindcss/plugin");
-const variables = require("./_variables.css.js");
+const variables = require("./variables.css.js");
 const fonts = require("./fonts.js");
 
 module.exports = plugin(function ({ addBase }) {
