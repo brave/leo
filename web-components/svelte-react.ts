eb30628336b9bd1506ae21e9d746cc33ef5bb2c4
Jay Harris <jay.harris@outlook.co.nz>
2022-08-29 16:01:37 +1200

    Auto infer event types from Svelte Component

--- web-components/svelte-react.ts
+++ web-components/svelte-react.ts
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect } from "react"
-import { SvelteComponent } from "svelte"
-import type {SvelteComponentTyped} from "svelte"
+import type { SvelteComponent, SvelteComponentTyped } from "svelte"
 
 const eventRegex = /on([A-Z]{1,}[a-zA-Z]*)/
 const watchRegex = /watch([A-Z]{1,}[a-zA-Z]*)/
@@ -9,6 +8,22 @@ const watchRegex = /watch([A-Z]{1,}[a-zA-Z]*)/
  * Type Utility for inferring the prop types of a Svelte component.
  */
 export type SvelteProps<T> = T extends SvelteComponentTyped<infer Props, any, any> ? Props : {};
+
+/**
+ * Type utility for inferring event types of a Svelte component.
+ */
+export type SvelteEvents<T> = T extends SvelteComponentTyped<any, infer Events, any> ? Events : {};
+
+/**
+ * Type utility for getting the React props of a Svelte component. This is all
+ * the Svelte props and the events mapped to the React `onEventName` format.
+ *
+ * For example, the event `click` would be mapped to `onClick`.
+ */
+export type SvelteToReactProps<SvelteComponent> = SvelteProps<SvelteComponent> & {
+  [Prop in keyof SvelteEvents<SvelteComponent> as `on${Capitalize<Prop & string>}`]?: SvelteEvents<SvelteComponent>[Prop];
+}
+
 
 // TODO(petemill): 
 // When web-components are supported in react (currently only in experimental version), then we can 
@@ -31,10 +46,10 @@ export type SvelteProps<T> = T extends SvelteComponentTyped<infer Props, any, an
  * @param component The imported svelte component itself. This is not used, but ensures that the component's code has been included in the bundle.
  * @returns A react component
  */
-export default function SvelteWebComponentToReact<Component extends SvelteComponent> (tag: string, component: { new(...args: any[]): Component }) {
-  return function ReactSvelteWebComponent (props: React.PropsWithChildren<SvelteProps<Component>>) {
+export default function SvelteWebComponentToReact<Component extends SvelteComponent>(tag: string, component: { new(...args: any[]): Component }) {
+  return function ReactSvelteWebComponent(props: React.PropsWithChildren<SvelteToReactProps<Component>>) {
     const component = useRef<SvelteComponent>()
-    
+
     const setRef = React.useCallback((ref: SvelteComponent) => {
       if (!ref) {
         console.error('No component for tag', tag)
@@ -51,7 +66,7 @@ export default function SvelteWebComponentToReact<Component extends SvelteCompon
         return
       }
       component.current = ref
-      
+
       // Events fire callbacks when the event is dispatched
       // from the svelte component.
       // Watchers fire callbacks when the variable (aka property or attribute)
