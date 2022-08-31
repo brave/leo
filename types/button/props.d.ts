1fe43c056b8da43fee7596e924965885c3160c48
Jay Harris <jay.harris@outlook.co.nz>
2022-09-01 09:24:18 +1200

    Add types

---
+++ types/button/props.d.ts
@@ -0,0 +1,4 @@
+export declare const buttonKinds: readonly ["CTA", "primary", "secondary", "tertiary"];
+export declare type ButtonKind = (typeof buttonKinds)[number];
+export declare const buttonSizes: readonly ["large", "medium", "small"];
+export declare type ButtonSize = (typeof buttonSizes)[number];
