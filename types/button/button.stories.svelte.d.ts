fb55926d69535253d5bfd3edc9dc312712142f08
Jay Harris <jay.harris@outlook.co.nz>
2022-09-01 09:24:18 +1200

    Add types

---
+++ types/button/button.stories.svelte.d.ts
@@ -0,0 +1,19 @@
+/** @typedef {typeof __propDef.props}  ButtonProps */
+/** @typedef {typeof __propDef.events}  ButtonEvents */
+/** @typedef {typeof __propDef.slots}  ButtonSlots */
+export default class Button extends SvelteComponentTyped<{}, {
+    [evt: string]: CustomEvent<any>;
+}, {}> {
+}
+export type ButtonProps = typeof __propDef.props;
+export type ButtonEvents = typeof __propDef.events;
+export type ButtonSlots = typeof __propDef.slots;
+import { SvelteComponentTyped } from "svelte";
+declare const __propDef: {
+    props: {};
+    events: {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {};
+};
+export {};
---
+++ types/button/react.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+/// <reference types="svelte/types/runtime/ambient" />
+/// <reference types="svelte2tsx/svelte-shims" />
+declare const _default: (props: any) => import("react").ReactElement<{
+    ref: (ref: import("*.svelte").default) => void;
+    children: import("react").ReactNode;
+}, string | import("react").JSXElementConstructor<any>>;
+export default _default;
