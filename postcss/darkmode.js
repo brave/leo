1bfe0c5aedbf6bfe351b558d6c6c4e22fb35549c
Jay Harris <jay.harris@outlook.co.nz>
2022-09-01 11:45:28 +1200

    Add postcss @darkmode plugin

---
+++ postcss/darkmode.js
@@ -0,0 +1,32 @@
+
+/**
+ * @type {import('postcss').PluginCreator}
+ */
+ module.exports = (opts = {}) => {
+    // Work with options here
+  
+    return {
+      postcssPlugin: '${pluginName}',
+      /*
+      Root (root, postcss) {
+        // Transform CSS AST here
+      }
+      */
+  
+      /*
+      Declaration (decl, postcss) {
+        // The faster way to find Declaration node
+      }
+      */
+  
+      /*
+      Declaration: {
+        color: (decl, postcss) {
+          // The fastest way find Declaration node if you know property name
+        }
+      }
+      */
+    }
+  }
+  
+  module.exports.postcss = true
