49ff7f3c89afaa4f413f11982a41458790173348
Jay Harris <jay.harris@outlook.co.nz>
2022-09-01 13:35:19 +1200

    Get the plugin working with Sass

--- postcss/darkmode.js
+++ postcss/darkmode.js
@@ -1,29 +1,35 @@
 const indent = (text, level) => text.split('\n')
-	.map(line => ''.padStart(level, '\t') + line)
-	.join('\n')
+    .map(line => ''.padStart(level, '\t') + line)
+    .join('\n')
 
-export default () => {
-  return {
-    postcssPlugin: 'darkmode',
-    AtRule: {
-      darkmode: atRule => {
-        const selectors = atRule.params.split(',').map(t => t.trim()).filter(s => s)
-        if (!selectors.length) selectors.push('&');
-        const body = atRule.nodes.join(';\n');
-        atRule.replaceWith(`
-@media (prefers-color-scheme: dark) {
-	${selectors.map(s => `${s}:not([data-theme="light"] ${s}):not([data-theme="light"])`).join(', ')} {
-${indent(body, 2)};
-	}
-}
-
-${selectors.map(s => `${s}[data-theme="dark"]`).join(', ')} {
-${indent(body, 1)}
-}`)
-        
-      }
+const renderRule = (rule, transform) => {
+    if (!rule.selector) {
+        console.log(rule.toString())
+        return ''
     }
-  }
+    const selectors = rule.selector.split(',').map(s => s.trim()).map(transform)
+    return `${selectors.join(', ')} {
+${indent(rule.nodes.map(n => n.toString() + ';').join('\n'), 1)}
+}`
 }
 
-export const postcss = true;
+module.exports = () => {
+    return {
+        postcssPlugin: 'darkmode',
+        AtRule: {
+            darkmode: atRule => {
+                const queryBody = atRule.nodes.map(n => renderRule(n, s => `${s}:not([data-theme=light] ${s}):not([data-theme=light])`)).join('\n\n');
+                const attributeBody = atRule.nodes.map(n => renderRule(n, s => `${s}[data-theme=dark]`)).join('\n\n')
+                const media = atRule.replaceWith(`
+@media (prefers-color-scheme: dark) {
+${indent(queryBody, 1)}
+}
+
+${attributeBody}
+`)
+            }
+        }
+    }
+}
+
+module.exports.postcss = true;
