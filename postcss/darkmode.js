018cdc9a39ea94d6edadc108a5ed3e6df07f1396
Jay Harris <jay.harris@outlook.co.nz>
2022-09-01 15:34:01 +1200

    Get the plugin working with data-theme

--- postcss/darkmode.js
+++ postcss/darkmode.js
@@ -13,20 +13,22 @@ ${indent(rule.nodes.map(n => n.toString() + ';').join('\n'), 1)}
 }`
 }
 
-module.exports = () => {
+module.exports = (options = { forceGlobal: true }) => {
     return {
         postcssPlugin: 'darkmode',
         AtRule: {
             darkmode: atRule => {
                 const queryBody = atRule.nodes.map(n => renderRule(n, s => `${s}:not([data-theme=light] ${s}):not([data-theme=light])`)).join('\n\n');
-                const attributeBody = atRule.nodes.map(n => renderRule(n, s => `${s}[data-theme=dark]`)).join('\n\n')
-                const media = atRule.replaceWith(`
+
+                const parentDarkSelector = options.forceGlobal ? `:global([data-theme=dark])` : `[data-theme=dark]`
+                const attributeBody = atRule.nodes.map(n => renderRule(n, s => `${s}[data-theme=dark], :global([data-theme=dark]) ${s}`)).join('\n\n')
+
+                atRule.replaceWith(`
 @media (prefers-color-scheme: dark) {
 ${indent(queryBody, 1)}
 }
 
-${attributeBody}
-`)
+${attributeBody}`)
             }
         }
     }
