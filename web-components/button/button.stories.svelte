936434e12f87bfdb0bf157a69710f2299ee44f3d
Jay Harris <jay.harris@outlook.co.nz>
2022-09-12 12:25:23 +1200

    Remove dark story - it looks too similar

--- web-components/button/button.stories.svelte
+++ web-components/button/button.stories.svelte
@@ -61,26 +61,3 @@
     <Button kind="tertiary" size="small" {...args}>Tertiary</Button>
   </div>
 </Story>
-
-<Story name="All Dark" let:args>
-  <h2 class="label">Large</h2>
-  <div data-theme=dark>
-    <div class="button-group">
-      <Button kind="primary" size="large" {...args}>Primary</Button>
-      <Button kind="secondary" size="large" {...args}>Secondary</Button>
-      <Button kind="tertiary" size="large" {...args}>Tertiary</Button>
-    </div>
-    <h2 class="label">Medium</h2>
-    <div class="button-group">
-      <Button kind="primary" size="medium" {...args}>Primary</Button>
-      <Button kind="secondary" size="medium" {...args}>Secondary</Button>
-      <Button kind="tertiary" size="medium" {...args}>Tertiary</Button>
-    </div>
-    <h2 class="label">Small</h2>
-    <div class="button-group">
-      <Button kind="primary" size="small" {...args}>Primary</Button>
-      <Button kind="secondary" size="small" {...args}>Secondary</Button>
-      <Button kind="tertiary" size="small" {...args}>Tertiary</Button>
-    </div>
-  </div>
-</Story>
