d1ce12bb355b4ba391fe8fdd61eab8ad8c5a3eea
Jacob Lamont <cob@jacoblamont.com>
2022-09-23 15:16:08 -0400

    fixes formatting

--- tokens/transformation/common/leoParser.js
+++ tokens/transformation/common/leoParser.js
@@ -7,26 +7,31 @@ const { removeKeyFromObject } = require("../../utils");
 module.exports = {
 	pattern: /\.json$/,
 	parse: ({ filePath, contents }) => {
-    // Replace emojies, e.g. '🌚 dark' :-)
-    contents = contents.replace(/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])\s?/gm, "")
+		// Replace emojies, e.g. '🌚 dark' :-)
+		contents = contents.replace(
+			/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])\s?/gm,
+			""
+		);
 		contents = JSON.parse(contents);
 
-    // Remove gradient|extended|gradient key repetition (do this before we remove 'extended'
-    // since we would end up with the path gradient|gradient and `removeKeyFromObject` would
-    // end up deleting everything under gradient).
-    contents = removeKeyFromObject(contents, 'gradient', false)
-    // Replace extended e.g. color|extended|primary-10
-    contents = removeKeyFromObject(contents, 'extended')
-    /**
-     * Convert layers from multiple tokens to single token with array of values.
-     * 
-     * Figma exports named gradients (e.g. gradient-gradient-04) with
-     * layered gradients as individual tokens (e.g. gradient-gradient-04-01, 
-     * gradient-gradient-02, etc.). This parser converts those layers to an
-     * array of values within the parent token (gradient-gradient-04).
-     */
-    const categories = Object.entries(contents);
-    for (const [category, categoryValue] of categories) {
+		// Remove gradient|extended|gradient key repetition (do this before we remove 'extended'
+		// since we would end up with the path gradient|gradient and `removeKeyFromObject` would
+		// end up deleting everything under gradient).
+		contents = removeKeyFromObject(contents, "gradient", false);
+
+		// Replace extended e.g. color|extended|primary-10
+		contents = removeKeyFromObject(contents, "extended");
+
+		/**
+		 * Convert layers from multiple tokens to single token with array of values.
+		 *
+		 * Figma exports named gradients (e.g. gradient-gradient-04) with
+		 * layered gradients as individual tokens (e.g. gradient-gradient-04-01,
+		 * gradient-gradient-02, etc.). This parser converts those layers to an
+		 * array of values within the parent token (gradient-gradient-04).
+		 */
+		const categories = Object.entries(contents);
+		for (const [category, categoryValue] of categories) {
 			const types = Object.entries(categoryValue);
 
 			for (const [type, typeValue] of types) {
@@ -41,7 +46,7 @@ module.exports = {
 				}
 
 				for (const [item, itemValue] of items) {
-          			// Combine items where figma splits a single-value to multiple values
+					// Combine items where figma splits a single-value to multiple values
 					if (["gradient", "elevation"].includes(type) && itemValue && !itemValue.type) {
 						contents[category][type][item] = groupValues(type, itemValue);
 					}
