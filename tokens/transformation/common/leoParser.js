d6821eb556c38f6de67eba71f47ee13d17e009ed
Jacob Lamont <cob@jacoblamont.com>
2022-09-22 22:30:02 -0400

    fixes transforms for Tailwind color tokens

--- tokens/transformation/common/leoParser.js
+++ tokens/transformation/common/leoParser.js
@@ -2,36 +2,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
-const merge = require('lodash.merge')
-
-/**
- * Removes a key and puts the children of that key in the key's parent
- *  e.g. remove "extended" in the path color|extended|primary-10.
- * Note: This cannot merge keys where a child has the same key as the parent,
- * e.g. we cannot remove the duplication of the path gradient|gradient|xyz.
- */
-function removeKeyFromObject(contents, keyToRemove, recurse=true) {
-  // Don't need to work on types without keys
-  if (typeof contents !== 'object') {
-    return contents
-  }
-  for (key in contents) {
-    if (key === keyToRemove) {
-      // Put matching children in their parent
-      // Parent may already have same key, so recursively merge
-      contents = merge(contents, contents[key])
-      delete contents[key]
-      // No more recursion - we are assuming that matching values
-      // will not also have descendent items with matching keys.
-    } else {
-      // Recurse
-      if (recurse) {
-        contents[key] = removeKeyFromObject(contents[key], keyToRemove, true)
-      }
-    }
-  }
-  return contents
-}
+const { removeKeyFromObject } = require("../../utils");
 
 module.exports = {
 	pattern: /\.json$/,
---
+++ tokens/utils.js
@@ -0,0 +1,34 @@
+const merge = require('lodash.merge')
+
+/**
+ * Removes a key and puts the children of that key in the key's parent
+ *  e.g. remove "extended" in the path color|extended|primary-10.
+ * Note: This cannot merge keys where a child has the same key as the parent,
+ * e.g. we cannot remove the duplication of the path gradient|gradient|xyz.
+ */
+function removeKeyFromObject(contents, keyToRemove, recurse=true) {
+  // Don't need to work on types without keys
+  if (typeof contents !== 'object') {
+    return contents
+  }
+  for (key in contents) {
+    if (key === keyToRemove) {
+      // Put matching children in their parent
+      // Parent may already have same key, so recursively merge
+      contents = merge(contents, contents[key])
+      delete contents[key]
+      // No more recursion - we are assuming that matching values
+      // will not also have descendent items with matching keys.
+    } else {
+      // Recurse
+      if (recurse) {
+        contents[key] = removeKeyFromObject(contents[key], keyToRemove, true)
+      }
+    }
+  }
+  return contents
+}
+
+module.exports = {
+    removeKeyFromObject
+}
