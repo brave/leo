382042c4f4a3a130eec23af3b14acf85838839ff
Jacob Lamont <cob@jacoblamont.com>
2022-09-23 11:35:29 -0400

    adds theme variations

--- .storybook/global.svelte
+++ .storybook/global.svelte
@@ -8,6 +8,23 @@
     background: var(--color-gray-20);
     color: var(--color-gray-80);
   }
+
+  h2 {
+    font-family: Arial, Helvetica, sans-serif;
+  }
 </style>
 
-<div class="layout"><slot /></div>
+<div class="layout">
+  <h2>User preference</h2>
+  <slot />
+</div>
+
+<div class="layout" data-theme="light">
+  <h2>Light</h2>
+  <slot />
+</div>
+
+<div class="layout" data-theme="dark">
+  <h2>Dark</h2>
+  <slot />
+</div>
