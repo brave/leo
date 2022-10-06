f6699cea92152c80b54ef6f5653ea8a92a9615aa
Jay Harris <jay.harris@outlook.co.nz>
2022-10-06 16:10:05 +1300

    Remove generated files

--- web-components/button/react.ts
+++
@@ -1,5 +0,0 @@
-import SvelteToReact, { ReactProps } from '../svelte-react';
-import Button from './button.svelte';
-import type { ButtonEvents, ButtonProps } from './button.svelte';
-
-export default SvelteToReact<ReactProps<ButtonProps, ButtonEvents>>('leo-button', Button);
