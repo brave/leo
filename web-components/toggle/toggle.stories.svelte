7ac3984970d05931a8f64e7f2d1b307d3cfdc7fe
Jay Harris <jay.harris@outlook.co.nz>
2022-11-28 16:04:52 +1300

    PoC story

--- web-components/toggle/toggle.stories.svelte
+++ web-components/toggle/toggle.stories.svelte
@@ -1,28 +1,54 @@
 <script lang="ts">
-    import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
-    import Toggle from './toggle.svelte'
-  
-    let on = false
-    function handleChange() {
-      on = !on
-    }
-  </script>
-  
-  <Meta
-    title="Toggle"
-    component={Toggle}
-    argTypes={{
-      dots: { control: 'number', defaultValue: 10 },
-      dotSize: { control: 'number', defaultValue: 8 },
-      activeColor: { control: 'color' },
-      defaultColor: { control: 'color' },
-    }}
-  />
-  
-  <Template let:args>
-    <div style={`--color-interaction-button-primary-background: ${args.activeColor || 'unset'}; --color-primary-20: ${args.defaultColor || 'unset'}`}>
-      <Toggle label="Toggle me!" on={on} />
-    </div>
-  </Template>
-  
-  <Story name="Default" />
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Toggle from './toggle.svelte'
+
+  let on = false
+  function handleChange() {
+    on = !on
+  }
+</script>
+
+<Meta
+  title="Toggle"
+  component={Toggle}
+  argTypes={{
+    toggleOnColor: { control: 'color' },
+    toggleOffColor: { control: 'color' },
+    toggleThumbColor: { control: 'color' },
+    togglePadding: { control: 'range', min: 0, max: 24, defaultValue: 2 },
+    toggleWidth: { control: 'range', min: 10, max: 200, defaultValue: 56 },
+    toggleHeight: { control: 'range', min: 10, max: 100, defaultValue: 32 }
+  }}
+/>
+
+<Template let:args>
+  <div
+    style={`
+        --leo-toggle-on-color: ${args.toggleOnColor || 'unset'};
+        --leo-toggle-off-color: ${args.toggleOffColor || 'unset'};
+        --leo-toggle-thumb-color: ${args.toggleThumbColor || 'unset'};
+        --leo-toggle-padding: ${args.togglePadding}px;
+        --leo-toggle-width: ${args.toggleWidth}px;
+        --leo-toggle-height: ${args.toggleHeight}px;
+    `}
+  >
+    <Toggle {on}>
+      <span slot="label">Toggle Me!</span>
+      <svg
+        slot="on-icon"
+        width="18"
+        height="18"
+        viewBox="0 0 18 18"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M14.6461 4.00015C14.2516 3.69959 13.6882 3.77573 13.3876 4.17021L7.47971 11.9243L4.53279 8.9774C4.18211 8.62672 3.61356 8.62672 3.26288 8.9774C2.91221 9.32807 2.91221 9.89663 3.26288 10.2473L6.93634 13.9208C7.11956 14.104 7.37301 14.1991 7.63155 14.1817C7.89009 14.1643 8.12851 14.0361 8.28555 13.83L14.8161 5.25861C15.1167 4.86413 15.0406 4.3007 14.6461 4.00015Z"
+          fill="currentColor"
+        />
+      </svg>
+    </Toggle>
+  </div>
+</Template>
+
+<Story name="Default" />
--- web-components/toggle/toggle.svelte
+++ web-components/toggle/toggle.svelte
@@ -4,7 +4,6 @@
   import { createEventDispatcher } from 'svelte'
 
   export let on: boolean
-  export let label: string = ''
 
   const dispatch = createEventDispatcher()
   const toggle = () => {
@@ -15,9 +14,12 @@
 
 <label class="leo-toggle">
   <button role="switch" aria-checked={on} part="track" on:click={toggle}>
-    <div class="thumb" part="thumb" />
+    <div class="thumb" part="thumb" aria-hidden="true">
+        <slot name="off-icon"/>
+        <slot name="on-icon"/>
+    </div>
   </button>
-  {label}
+  <slot name="label"></slot>
 </label>
 
 <style lang="scss">
@@ -25,38 +27,60 @@
     --leo-toggle-height: 32px;
     --leo-toggle-width: 56px;
     --leo-toggle-padding: 2px;
+    --leo-toggle-on-color: var(--color-interaction-button-primary-background);
+    --leo-toggle-off-color: var(--color-gray-30);
+    --leo-toggle-thumb-color: var(--color-white);
   }
 
   .leo-toggle {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    align-items: center;
 
     & button {
       all: unset;
-      background: var(--color-gray-30);
+      background: var(--leo-toggle-off-color);
       width: var(--leo-toggle-width);
       height: var(--leo-toggle-height);
       border-radius: var(--radius-full);
       padding: var(--leo-toggle-padding);
       transition: background-color 0.2s ease-in-out;
+      flex-shrink: 0;
 
       .thumb {
         height: 100%;
         aspect-ratio: 1/1;
         background: white;
         border-radius: var(--radius-full);
-        transition: transform 0.2s ease-in-out;
+        transition: transform 0.2s ease-in-out, color 0.2s ease-in-out;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        :global([slot$=-icon]) {
+            transition: opacity 0.2s ease-in-out;
+        }
+
+        :global([slot=on-icon]) {
+            opacity: 0;
+        }
       }
 
       &[aria-checked='true'] {
-        background: var(--color-interaction-button-primary-background);
+        background: var(--leo-toggle-on-color);
 
         .thumb {
           transform: translate(
             calc(var(--leo-toggle-width) - var(--leo-toggle-height) + 0.25px),
             0
           );
+          color: var(--leo-toggle-on-color);
+        }
+
+        :global([slot=on-icon]) {
+            opacity: 1;
         }
       }
     }
