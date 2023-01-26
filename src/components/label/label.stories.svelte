19beb22960948199f66c8f52f7c3db090f30dce4
Jay Harris <jay.harris@outlook.co.nz>
2023-01-26 14:46:59 +1300

    Make more customizable

--- src/components/label/label.stories.svelte
+++ src/components/label/label.stories.svelte
@@ -1,7 +1,8 @@
 <script>
   import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Icon from '../icon/icon.svelte'
 
-  import Label from './label.svelte'
+  import Label, { colors, modes } from './label.svelte'
 </script>
 
 <Meta
@@ -9,19 +10,68 @@
   component={Label}
   argTypes={{
     color: {
-      control: 'color',
+      control: 'select',
       description: 'The color to use for the icon',
-      type: 'string',
+      options: colors,
+      defaultValue: 'teal'
     },
     mode: {
       control: 'select',
-      options: ['faint', 'heavy'],
+      options: modes,
+      defaultValue: 'heavy'
+    },
+    leftIcon: {
+      control: 'text',
+      description: 'Icon to the left',
+      type: 'text',
+      defaultValue: 'check-circle-outline',
+    },
+    rightIcon: {
+      control: 'text',
+      description: 'Icon to the right',
+      type: 'text',
+      defaultValue: 'check-circle-outline',
     },
   }}
 />
 
 <Template let:args>
-  <Label {...args} />
+  <Label {...args}>
+    {#if args.leftIcon}
+      <Icon name={args.leftIcon} />
+    {/if}
+    Label
+    {#if args.rightIcon}
+      <Icon name={args.rightIcon} />
+    {/if}
+  </Label>
 </Template>
 
+<Story name="All" let:args>
+  <div class="column">
+    {#each colors as color}
+      {#each modes as mode}
+        <Label {color} {mode}>
+          {#if args.leftIcon}
+            <Icon name={args.leftIcon} />
+          {/if}
+          {mode}
+          {color}
+          {#if args.rightIcon}
+            <Icon name={args.rightIcon} />
+          {/if}
+        </Label>
+      {/each}
+    {/each}
+  </div>
+</Story>
 <Story name="Default" />
+
+<style lang="scss">
+  .column {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-4);
+    align-items: flex-start;
+  }
+</style>
--- src/components/label/label.svelte
+++ src/components/label/label.svelte
@@ -1,35 +1,67 @@
 <script context="module" lang="ts">
-    const colors = {
-        
-    }
-</script>
-<script lang="ts">
-    export let mode: 'faint' | 'heavy' = 'faint'
-    export let color: 'gray' | 'secondary' | 'primary' | 'red' | 'yellow' | 'green' | 'blue' | 'purple' | 'pink' | 'teal' | 'hero' = 'gray'
+  export const colors = [
+    'gray',
+    'secondary',
+    'primary',
+    'red',
+    'yellow',
+    'green',
+    'blue',
+    'purple',
+    'pink',
+    'teal',
+    'hero',
+  ] as const
+  export type Color = typeof colors[number]
 
-    $: background = color === 'hero'
-        ? `var(--gradient-hero)`
-        : mode === 'heavy'
-            ? `var(--color-${color}-40)`
-            : `var(--color-${color}-10)`
-    $: text = color === 'hero'
-        ? `var(--color-white)`
-        : mode === 'heavy'
-            ? `var(--color-${color}-10)`
-            : `var(--color-${color}-50)`
+  export const modes = ['faint', 'heavy'] as const
+  export type Mode = typeof modes[number]
 </script>
+
+<script lang="ts">
+  export let mode: Mode = 'faint'
+  export let color: Color = 'gray'
+
+  $: background =
+    color === 'hero'
+      ? `var(--gradient-hero)`
+      : mode === 'heavy'
+      ? `var(--color-${color}-40)`
+      : `var(--color-${color}-10)`
+  $: text =
+    color === 'hero'
+      ? `var(--color-white)`
+      : mode === 'heavy'
+      ? `var(--color-${color}-10)`
+      : `var(--color-${color}-50)`
+</script>
+
+<div class="leo-label" style:background style:color={text}>
+  <div class="content">
+    <slot>Label</slot>
+  </div>
+</div>
 
 <style lang="scss">
-    .leo-label {
-        --font-text: var(--leo-label-font-text, var(--font-text-x-small-semibold));
-        display: inline;
-        border-radius: var(--radius-4);
-        padding: var(--spacing-4);
+  .leo-label {
+    --icon-size: var(--leo-label-icon-size, 13px);
+    --font-text: var(--leo-label-font-text, var(--font-text-x-small-semibold));
+    --padding: var(--leo-label-padding, var(--spacing-4));
+    --radius: var(--leo-label-radius, var(--radius-4));
 
-        font: var(--font-text);
-    }
+    --leo-icon-size: var(--icon-size);
+
+    display: inline-block;
+    border-radius: var(--radius);
+    padding: var(--padding);
+    font: var(--font-text);
+  }
+
+  .content {
+    display: flex;
+    flex-direction: row;
+    gap: 4px;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
-
-<div class="leo-label" style:background={background} style:color={text}>
-    <slot>Label</slot>
-</div>
