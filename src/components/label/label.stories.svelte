ec050214ebece2e503a85a4b5c3a9db0fa2ae00a
Jay Harris <jay.harris@outlook.co.nz>
2023-01-26 14:40:36 +1300

    Start work on Label

---
+++ src/components/label/label.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+
+  import Label from './label.svelte'
+</script>
+
+<Meta
+  title="Label"
+  component={Label}
+  argTypes={{
+    color: {
+      control: 'color',
+      description: 'The color to use for the icon',
+      type: 'string',
+    },
+    mode: {
+      control: 'select',
+      options: ['faint', 'heavy'],
+    },
+  }}
+/>
+
+<Template let:args>
+  <Label {...args} />
+</Template>
+
+<Story name="Default" />
---
+++ src/components/label/label.svelte
@@ -0,0 +1,32 @@
+<script context="module" lang="ts">
+    const colors = {
+        
+    }
+</script>
+<script lang="ts">
+    export let mode: 'faint' | 'heavy' = 'faint'
+    export let color: 'gray' | 'secondary' | 'primary' | 'red' | 'yellow' | 'green' | 'blue' | 'purple' | 'pink' | 'teal' | 'hero' = 'gray'
+
+    $: background = color === 'hero'
+        ? `var(--gradient-hero)`
+        : mode === 'heavy'
+            ? `var(--color-${color}-40)`
+            : `var(--color-${color}-10)`
+    $: text = color === 'hero'
+        ? `var(--color-white)`
+        : mode === 'heavy'
+            ? `var(--color-${color}-10)`
+            : `var(--color-${color}-50)`
+</script>
+
+<style lang="scss">
+    .leo-label {
+        display: inline;
+        border-radius: var(--radius-4);
+        padding: var(--spacing-4);
+    }
+</style>
+
+<div class="leo-label" style:background={background} style:color={text}>
+    <slot>Label</slot>
+</div>
