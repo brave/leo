adba315f428c31869dd30565d7f37f81386a77a0
Jay Harris <jay.harris@outlook.co.nz>
2023-02-02 13:01:33 +1300

    Add progress bar

--- src/components/progress/progressBar.svelte
+++ src/components/progress/progressBar.svelte
@@ -1,5 +1,31 @@
 <svelte:options tag="leo-progressbar" />
+
 <script lang="ts">
-    export let progress: number
+  export let progress: number = 0
 </script>
-<div>Bar</div>
+
+<div class="leo-progressbar">
+  <div class="progress" style="width: {progress * 100}%" />
+</div>
+
+<style lang="scss">
+  .leo-progressbar {
+    --radius: var(--leo-progressbar-radius, 10px);
+    --transition-duration: var(--leo-progressbar-transition-duration, 0.2s);
+    background: var(--leo-progressbar-background, var(--color-primary-20));
+    height: var(--leo-progressbar-height, 4px);
+    border-radius: var(--radius);
+
+    .progress {
+      height: 100%;
+      width: 100%;
+      transition: width var(--transition-duration) ease-in-out;
+      background: var(
+        --leo-progressbar-progress,
+        var(--color-interaction-button-primary-background)
+      );
+      border-radius: var(--radius);
+
+    }
+  }
+</style>
