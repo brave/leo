ebb4f7fb9e6684323616fccfebd2ca50b93cbf59
Jay Harris <jay.harris@outlook.co.nz>
2023-02-09 10:49:28 +1300

    [Component]: Link (#194)
    * Add link component

---
+++ src/components/link/link.stories.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+
+  import Link from './link.svelte'
+</script>
+
+<Meta
+  title="Link"
+  component={Link}
+  argTypes={{
+    href: {
+      control: 'text',
+      description: 'The href for the link',
+      type: 'string',
+      defaultValue: 'https://brave.com'
+    },
+    target: {
+      control: 'text',
+      description: 'The target for the link',
+      type: 'string',
+      defaultValue: '_blank'
+    },
+    text: {
+      control: 'text',
+      description: 'The text content of the link',
+      type: 'string',
+      defaultValue: 'Download Brave'
+    }
+  }}
+/>
+
+<Template let:args>
+  <div style={`color: ${args.color}; --leo-icon-size: ${args.size}px;`}>
+    <Link href="#foo" {...args} />
+  </div>
+</Template>
+
+<Story name="Default" />
---
+++ src/components/link/link.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import type { SvelteHTMLElements } from 'svelte/elements'
+
+  type ExcludedProps = 'class' | 'href' | 'aria-disabled'
+  type $$Props = Omit<Partial<SvelteHTMLElements['a']>, ExcludedProps> & {
+    isDisabled?: boolean
+    href: string
+  }
+
+  export let href: string
+  export let isDisabled: boolean = false
+</script>
+
+<a
+  rel="noopener"
+  {...$$restProps}
+  {href}
+  class="leo-link"
+  class:disabled={isDisabled}
+  aria-disabled={isDisabled}
+  on:click={(e) => {
+    if (isDisabled) e.preventDefault()
+  }}
+>
+  <slot />
+</a>
+
+<style lang="scss">
+  a {
+    all: unset;
+
+    --color: var(--leo-link-color, var(--color-text-interactive));
+    --hover-color: var(--leo-link-hover-color, var(--color-primary-60));
+    --visited-color: var(--leo-link-visited-color, var(--color-pink-50));
+    --disabled-color: var(
+      --leo-link-disabled-color,
+      var(--color-text-disabled)
+    );
+    --focus-color: var(--leo-link-focus-color, var(--color));
+    --focus-shadow: var(--leo-link-focus-shadow, var(--effect-focus-state));
+
+    color: var(--color);
+    font: var(--font-text-default-semibold);
+    cursor: pointer;
+    text-decoration: underline;
+
+    &:hover {
+      color: var(--hover-color);
+    }
+
+    &:visited:not(:hover) {
+      color: var(--visited-color);
+    }
+
+    &:focus-visible:not(.disabled) {
+      color: var(--focus-color);
+      box-shadow: var(--focus-shadow);
+    }
+
+    &.disabled {
+      color: var(--disabled-color);
+      pointer-events: none;
+    }
+  }
+</style>
