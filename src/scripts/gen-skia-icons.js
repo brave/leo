cd2a007d99fd706bf040b9bc78a369cc69435cc5
Jay Harris <jay.harris@outlook.co.nz>
2023-02-23 11:06:53 +1300

    Include Skiafy in repo

--- src/scripts/gen-skia-icons.js
+++ src/scripts/gen-skia-icons.js
@@ -1,30 +1,26 @@
 const fs = require('fs/promises')
 const path = require('path')
+const skiafy = require('./icons/skiafy')
 
 const ICONS_FOLDER = 'icons/'
 const OUTPUT_FOLDER = 'icons-skia/'
 
-import('skiafy').then(async ({ default: skiafy }) => {
-  await fs.mkdir(OUTPUT_FOLDER, { recursive: true })
-
-  const skiafyPromise = await fs
-    .readdir(ICONS_FOLDER)
+fs.mkdir(OUTPUT_FOLDER, { recursive: true })
+    .then(() => fs.readdir(ICONS_FOLDER))
     .then((icons) => icons.filter((i) => i.endsWith('.svg')))
     .then((icons) =>
-      icons.map(async (icon) => {
-        const svgContent = await fs.readFile(path.join(ICONS_FOLDER, icon), {
-          encoding: 'utf8'
+        icons.map(async (icon) => {
+            const svgContent = await fs.readFile(path.join(ICONS_FOLDER, icon), {
+                encoding: 'utf8'
+            })
+            const skiaContent = skiafy(svgContent)
+            const outputFileName = `leo_${path
+                .basename(icon, '.svg')
+                .replace('-', '_')}.icon`
+            await fs.writeFile(
+                path.join(OUTPUT_FOLDER, outputFileName),
+                skiaContent
+            )
         })
-        const skiaContent = skiafy(svgContent)
-        const outputFileName = `leo_${path
-          .basename(icon, '.svg')
-          .replace('-', '_')}.icon`
-        await fs.writeFile(
-          path.join(OUTPUT_FOLDER, outputFileName),
-          skiaContent
-        )
-      })
     )
 
-  await Promise.all(skiafyPromise)
-})
