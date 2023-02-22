548d29345f5aeb64e0322b45cea704a6d665fdea
Jay Harris <jay.harris@outlook.co.nz>
2023-02-23 11:30:35 +1300

    npm run format

--- src/scripts/gen-skia-icons.js
+++ src/scripts/gen-skia-icons.js
@@ -6,21 +6,17 @@ const ICONS_FOLDER = 'icons/'
 const OUTPUT_FOLDER = 'icons-skia/'
 
 fs.mkdir(OUTPUT_FOLDER, { recursive: true })
-    .then(() => fs.readdir(ICONS_FOLDER))
-    .then((icons) => icons.filter((i) => i.endsWith('.svg')))
-    .then((icons) =>
-        icons.map(async (icon) => {
-            const svgContent = await fs.readFile(path.join(ICONS_FOLDER, icon), {
-                encoding: 'utf8'
-            })
-            const skiaContent = skiafy(svgContent)
-            const outputFileName = `leo_${path
-                .basename(icon, '.svg')
-                .replace('-', '_')}.icon`
-            await fs.writeFile(
-                path.join(OUTPUT_FOLDER, outputFileName),
-                skiaContent
-            )
-        })
-    )
-
+  .then(() => fs.readdir(ICONS_FOLDER))
+  .then((icons) => icons.filter((i) => i.endsWith('.svg')))
+  .then((icons) =>
+    icons.map(async (icon) => {
+      const svgContent = await fs.readFile(path.join(ICONS_FOLDER, icon), {
+        encoding: 'utf8'
+      })
+      const skiaContent = skiafy(svgContent)
+      const outputFileName = `leo_${path
+        .basename(icon, '.svg')
+        .replace('-', '_')}.icon`
+      await fs.writeFile(path.join(OUTPUT_FOLDER, outputFileName), skiaContent)
+    })
+  )
