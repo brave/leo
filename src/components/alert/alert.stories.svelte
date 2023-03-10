27ed07fbc57dedcfa5143db70fa00c8718663acd
Jay Harris <jay.harris@outlook.co.nz>
2023-03-10 16:28:26 +1300

    PoC alerts

---
+++ src/components/alert/alert.stories.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Button from '../button/button.svelte'
+
+  import Alert, { modes, types } from './alert.svelte'
+</script>
+
+<Meta
+  title="Alert"
+  component={Alert}
+  argTypes={{
+    mode: { control: 'select', options: modes },
+    type: { control: 'select', options: types }
+  }}
+/>
+
+<Template let:args>
+  <Alert {...args}>
+    <div slot="title">Title</div>
+    Alert content
+    <div slot="actions">
+      <Button kind="tertiary">Secondary</Button>
+      <Button kind="primary">Primary</Button>
+    </div>
+  </Alert>
+</Template>
+
+<Story name="Alert" />
+<Story name="All" let:args>
+  <div class="container">
+    {#each modes as mode}
+      {#each types as type}
+        <Alert {mode} {type} {...args}>
+          <div slot="title">Title</div>
+          Alert content
+          <div slot="actions">
+            <Button kind="tertiary">Secondary</Button>
+            <Button kind="primary">Primary</Button>
+          </div>
+        </Alert>
+      {/each}
+    {/each}
+  </div>
+</Story>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    max-width: 500px;
+  }
+</style>
---
+++ src/components/alert/alert.svelte
@@ -0,0 +1,101 @@
+<svelte:options tag="leo-alert" />
+
+<script lang="ts" context="module">
+  export const types = ['info', 'warning', 'error', 'success'] as const
+  export type AlertType = (typeof types)[number]
+
+  export const modes = ['toast', 'callout'] as const
+  export type AlertMode = (typeof modes)[number]
+
+  const defaultIcons: { [P in AlertType]: string } = {
+    'info': 'info-filled',
+    'error': 'warning-circle-filled',
+    'warning': 'warning-triangle-filled',
+    'success': 'check-circle-filled'
+  }
+</script>
+
+<script lang="ts">
+  import Icon from '../icon/icon.svelte'
+
+  export let type: AlertType = 'error'
+  export let mode: AlertMode = 'toast'
+</script>
+
+<div
+  class="leo-alert"
+  class:toast={mode === 'toast'}
+  class:callout={mode === 'callout'}
+  style:--default-background={`var(--leo-color-systemfeedback-${type}-background)`}
+  style:--default-icon-color={`var(--leo-color-systemfeedback-${type}-icon)`}
+>
+  <div class="icon">
+    <slot name="icon">
+      <Icon name={defaultIcons[type]} />
+    </slot>
+  </div>
+  <div class="content">
+    <div class="title">
+      {#if mode == 'callout'}
+        <slot name="title" />
+      {/if}
+    </div>
+    <slot />
+  </div>
+  <div class="actions">
+    <slot name="actions" />
+  </div>
+</div>
+
+<style lang="scss">
+  .leo-alert {
+    --leo-icon-color: var(--leo-alert-icon-color, var(--default-icon-color));
+    background-color: var(
+      --leo-alert-background-color,
+      var(--default-background)
+    );
+    display: flex;
+    color: var(--leo-color-text-primary);
+    padding: var(--leo-spacing-16);
+    border-radius: var(--leo-radius-8);
+    gap: var(--leo-spacing-16);
+    font: var(--leo-font-primary-default-regular);
+
+    & .icon {
+      color: var(--leo-icon-color);
+    }
+
+    & .title {
+      font: var(--leo-font-heading-h4);
+    }
+
+    & :global(.actions > *),
+    :global(.actions::slotted(*)) {
+      display: flex;
+      flex-direction: row;
+      gap: var(--leo-spacing-16);
+    }
+  }
+
+  .leo-alert.toast {
+    --leo-icon-size: 16px;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    & .content {
+      flex: 1;
+    }
+  }
+
+  .leo-alert.callout {
+    --leo-icon-size: 26px;
+    display: grid;
+    grid-template-columns: min-content auto;
+    grid-template-rows: auto min-content;
+
+    & .actions {
+      grid-column: 2;
+    }
+  }
+</style>
