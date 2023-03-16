255a7d6faa63aec61746108b6a0da40af3ad47d3
Jay Harris <jay.harris@outlook.co.nz>
2023-03-16 14:09:18 +1300

    Semi working attributes

--- src/components/svelte-web.ts
+++ src/components/svelte-web.ts
@@ -1,3 +1,5 @@
+import type { SvelteComponent } from 'svelte'
+
 interface Options {
     mode: 'open' | 'closed'
     name: string
@@ -5,14 +7,43 @@ interface Options {
 }
 
 export default function registerWebComponent(component: ConstructorOfATypedSvelteComponent, { name, mode }: Options) {
+    const c = new component({ target: document.createElement('div') }) as any
+    const props = Object.keys(c.$$.props)
+    const attributePropMap = new Map<string, string>();
+    for (const prop of props) {
+        attributePropMap.set(prop.toLowerCase(), prop);
+    }
+    const attributes = Array.from(attributePropMap.keys())
+
     class SvelteWrapper extends HTMLElement {
+        component: SvelteComponent
+
+        static get observedAttributes() {
+            return attributes;
+        }
+
         constructor() {
             super()
 
             const shadow = this.attachShadow({ mode })
-            new component({
+            this.component = new component({
                 target: shadow
-            })
+            }) as any
+
+            // console.log(name, component)
+        }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+            console.log(name, oldValue, newValue);
+            (this.component as any).$$set({ [attributePropMap.get(name)]: newValue })
+        }
+
+        connectedCallback() {
+            console.log('connected')
+        }
+
+        disconnectedCallback() {
+            console.log('disconnected')
         }
     }
 
