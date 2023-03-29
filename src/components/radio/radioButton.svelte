d088aa079e342a0c59351a98620b71a79643131b
Jay Harris <jay.harris@outlook.co.nz>
2023-03-30 12:32:09 +1300

    Make the radioButton work inside a customElement

--- src/components/radio/radioButton.svelte
+++ src/components/radio/radioButton.svelte
@@ -5,10 +5,24 @@
   export let value: string | number | any
   export let name: string
 
+  const tagName = 'leo-radiobutton'
   const dispatch = createEventDispatcher()
 
   function changed(e) {
     if (!e.target.checked) return
+
+    // If we're in a custom element, make sure we update all the
+    // other elements in our group, to make the behavior the same
+    // as the built in radio.
+    const maybeElement = e.target.getRootNode()?.host
+    if (maybeElement && maybeElement.tagName === tagName.toUpperCase()) {
+      // Note: We query the rootNode containing the element so we work
+      // even when our element is contained inside another shadowRoot.
+      const elements = maybeElement
+        .getRootNode()
+        .querySelectorAll(`${tagName}[name=${name}]`)
+      for (const el of elements) el.currentValue = value
+    }
 
     currentValue = value
     dispatch('changed', { value })
