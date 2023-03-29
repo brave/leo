580953411abb2a8765f9eb2be90575f78cdbe737
Jay Harris <jay.harris@outlook.co.nz>
2023-03-30 12:22:28 +1300

    Begin work on Radio button

---
+++ src/components/radio/radio.stories.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import { getStyleFromArgs } from '../../../.storybook/argHelper'
+  import RadioButton from './radioButton.svelte'
+
+  let currentValue = 'hello'
+  let container
+
+  let button = 0
+
+  // Number of radios per template - we do this here because the template is
+  // duplicated for light/dark/user pref and we don't want them to be in the
+  // same group.
+  let numRadios = 4
+</script>
+
+<Meta
+  title="Radio"
+  component={RadioButton}
+  argTypes={{
+    name: { control: 'text', defaultValue: `options` }
+  }}
+/>
+
+<Template let:args>
+  <div bind:this={container} style={getStyleFromArgs(args)}>
+    <RadioButton
+      {...args}
+      name={args.name + Math.floor(button++ / numRadios)}
+      bind:currentValue
+      value={'hello'}>Hello</RadioButton
+    >
+    <RadioButton
+      {...args}
+      name={args.name + Math.floor(button++ / numRadios)}
+      bind:currentValue
+      value={'world'}
+    />
+    <RadioButton
+      {...args}
+      name={args.name + Math.floor(button++ / numRadios)}
+      bind:currentValue
+      value={'foo'}
+    />
+    <RadioButton
+      {...args}
+      name={args.name + Math.floor(button++ / numRadios)}
+      bind:currentValue
+      value={'bar'}
+    />
+    <div>Current: {currentValue}</div>
+  </div>
+</Template>
+
+<Story name="Default" />
---
+++ src/components/radio/radioButton.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
+  export let currentValue: string | number | any
+  export let value: string | number | any
+  export let name: string
+
+  const dispatch = createEventDispatcher()
+
+  function changed(e) {
+    if (!e.target.checked) return
+
+    currentValue = value
+    dispatch('changed', { value })
+  }
+</script>
+
+<label class="leo-radioButton">
+  <input
+    type="radio"
+    {name}
+    checked={currentValue === value}
+    on:change={changed}
+  />
+  <slot>{value}</slot>
+</label>
+
+<style lang="scss">
+  .leo-radioButton {
+  }
+</style>
