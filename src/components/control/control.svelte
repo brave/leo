68eed95e5c5f9280d753b11c36c45da0260909e9
Jay Harris <jay.harris@outlook.co.nz>
2023-04-28 15:06:42 +1200

    More dropdown styling

--- src/components/control/control.svelte
+++ src/components/control/control.svelte
@@ -2,28 +2,42 @@
 </script>
 
 <div class="leo-control" on:click on:keyup>
-    <slot name="left-icon"/>
-    <slot/>
-    <slot name="right-icon"/>
+  <slot name="left-icon" />
+  <slot />
+  <slot name="right-icon" />
 </div>
 
 <style lang="scss">
-    .leo-control {
-        --radius: var(--leo-control-radius, var(--leo-spacing-8));
-        --background: var(--leo-control-background, var(--leo-color-container-highlight));
-        --padding: var(--leo-control-padding, var(--leo-spacing-12) var(--leo-spacing-8));
-        --font: var(--leo-control-font, var(--leo-font-primary-default-regular));
+  .leo-control {
+    --radius: var(--leo-control-radius, var(--leo-spacing-8));
+    --background: var(
+      --leo-control-background,
+      var(--leo-color-container-highlight)
+    );
+    --padding: var(
+      --leo-control-padding,
+      var(--leo-spacing-12) var(--leo-spacing-8)
+    );
+    --font: var(--leo-control-font, var(--leo-font-primary-default-regular));
 
-        border-radius: var(--radius);
-        background: var(--background);
-        padding: var(--padding);
-        font: var(--font);
+    border-radius: var(--radius);
+    background: var(--background);
+    padding: var(--padding);
+    font: var(--font);
 
-        border: 1px solid transparent;
+    border: 1px solid transparent;
 
-        &:hover {
-            border-color: var(--leo-color-divider-subtle);
-            /* border: 1px var(--leo-color-divider-subtle); */
-        }
+    display: flex;
+    flex-direction: row;
+    gap: var(--leo-spacing-8);
+
+    &:hover {
+      border-color: var(--leo-color-divider-subtle);
     }
+
+    &:focus-within {
+      box-shadow: 0px 0px 0px 1.5px rgba(255, 255, 255, 0.5),
+        0px 0px 4px 2px #423eee;
+    }
+  }
 </style>
--- src/components/dropdown/dropdown.svelte
+++ src/components/dropdown/dropdown.svelte
@@ -23,14 +23,18 @@
 <div class="leo-dropdown">
   <Control on:click={(e) => (isOpen = !isOpen)}>
     <slot name="left-icon" slot="left-icon" />
-    <span class="value">{value}</span>
+    <button>
+      <span class="value">{value}</span>
+    </button>
     <slot name="right-icon" slot="right-icon" />
   </Control>
   <div class="menu">
     {#if isOpen}
       <div class="popup" use:clickOutside={(e) => (isOpen = false)}>
         {#each options as option}
-          <button on:click={(e) => selectOption(option)}>{option}</button>
+          <button class="popup-item" on:click={(e) => selectOption(option)}
+            >{option}</button
+          >
         {/each}
       </div>
     {/if}
@@ -39,6 +43,13 @@
 
 <style lang="scss">
   .leo-dropdown {
+    --dropdown-gap: var(--leo-dropdown-gap, var(--leo-spacing-8));
+    --font: var(--leo-dropdown-font, var(--leo-font-primary-default-regular));
+
+    font: var(--font);
+    cursor: pointer;
+    -webkit-tap-highlight-color: transparent;
+
     button {
       all: unset;
     }
@@ -53,14 +64,45 @@
   }
 
   .leo-dropdown .popup {
-    background: white;
-    box-shadow: var(--leo-effect-elevation-04);
+    background: var(--leo-color-container-background);
+    box-shadow: var(--leo-effect-elevation-03);
     position: absolute;
-    top: 0;
+    top: var(--dropdown-gap);
     left: 0;
     right: 0;
 
     display: flex;
     flex-direction: column;
+    border-radius: var(--leo-radius-8);
+    max-height: 200px;
+    overflow-y: auto;
+    overflow-x: visible;
+    border: 1px solid var(--leo-color-divider-subtle);
+  }
+
+  .leo-dropdown .popup-item {
+    padding: var(--leo-spacing-16);
+
+    &:hover {
+      background: var(--leo-color-container-interactive-background);
+      color: var(--leo-color-text-interactive);
+    }
+
+    &:active {
+      background: var(--leo-color-primary-20);
+      color: var(--leo-color-text-interactive);
+    }
+
+    &:focus-visible {
+      /** Our glow won't be visible if it's outside the parent, so shrink the
+        * padding a little bit so the glow fits inside */
+      --glow-size: 3px;
+      padding: calc(var(--leo-spacing-16) - var(--glow-size));
+      margin: var(--glow-size);
+
+      border-radius: var(--leo-spacing-8);
+      box-shadow: 0px 0px 0px 1.5px rgba(255, 255, 255, 0.5),
+        0px 0px 4px 2px #423eee;
+    }
   }
 </style>
