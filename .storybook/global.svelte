fc396d52244e8571af4a77d310923ff6a415be07
Jay Harris <jay.harris@outlook.co.nz>
2023-06-13 10:47:11 +1200

    Improve storybook layout

--- .storybook/global.svelte
+++ .storybook/global.svelte
@@ -1,34 +1,44 @@
 <script lang="ts">
   import AlertCenter from '../src/components/alert/alertCenter.svelte'
+  import Icon from '../src/components/icon/icon.svelte'
+  import Toggle from '../src/components/toggle/toggle.svelte'
   import '../tokens/css/variables.css'
+
+  let theme = localStorage.getItem('theme') ?? 'light'
+  document.body.setAttribute('data-theme', theme)
 </script>
 
 <AlertCenter />
 
 <div class="layout">
-  <h2>User preference</h2>
-  <slot />
-</div>
-
-<div class="layout" data-theme="light">
-  <h2>Light</h2>
-  <slot />
-</div>
-
-<div class="layout" data-theme="dark">
-  <h2>Dark</h2>
+  <div class="theme-toggle-container">
+    <Toggle size="small" checked={theme === 'light'} on:change={() => {
+      theme = theme === 'light' ? 'dark' : 'light'
+      document.body.setAttribute('data-theme', theme)
+      localStorage.setItem('theme', theme)
+    }}>
+      {theme[0].toUpperCase() + theme.slice(1)}
+      <Icon slot="on-icon" name="theme-light" />
+    </Toggle>
+  </div>
   <slot />
 </div>
 
 <style>
-  .layout {
-    padding: 30px;
+  :global body {
     background: var(--leo-color-container-background);
     color: var(--leo-color-text-primary);
     font: var(--leo-font-primary-default-regular);
   }
 
-  h2 {
-    font-family: Arial, Helvetica, sans-serif;
+  .layout {
+    padding: 30px;
+  }
+
+  .theme-toggle-container {
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    margin-bottom: var(--leo-spacing-8);
   }
 </style>
