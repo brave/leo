ee1b16c2afd7d7c3e759a790127e99569dec069c
Jay Harris <jay.harris@outlook.co.nz>
2023-06-13 15:01:43 +1200

    Fix theming when we have multiple stories on the same page

--- .storybook/global.svelte
+++ .storybook/global.svelte
@@ -4,13 +4,16 @@
   import Toggle from '../src/components/toggle/toggle.svelte'
   import '../tokens/css/variables.css'
 
+  // Note: We set the data-theme attribute on the body and on our layout element
+  // so that the theme is correctly set whether there are multiple stories on
+  // the page or just this one.
   let theme = localStorage.getItem('theme') ?? 'light'
   document.body.setAttribute('data-theme', theme)
 </script>
 
 <AlertCenter />
 
-<div class="layout">
+<div class="layout" data-theme={theme}>
   <div class="theme-toggle-container">
     <Toggle size="small" checked={theme === 'light'} on:change={() => {
       theme = theme === 'light' ? 'dark' : 'light'
@@ -25,7 +28,7 @@
 </div>
 
 <style>
-  :global body {
+  :global body, .layout {
     background: var(--leo-color-container-background);
     color: var(--leo-color-text-primary);
     font: var(--leo-font-primary-default-regular);
