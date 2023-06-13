5f1b043aa03c87e378473b9c56ccb2fcd1a2de4e
Jay Harris <jay.harris@outlook.co.nz>
2023-06-13 14:44:31 +1200

    Add slots for collapse

--- src/components/collapse/collapse.stories.svelte
+++ src/components/collapse/collapse.stories.svelte
@@ -4,6 +4,8 @@
   import Icon from '../icon/icon.svelte'
 
   import Collapse from './collapse.svelte'
+  import SlotInfo from '../../storyHelpers/SlotInfo.svelte'
+  import Slot from '../../storyHelpers/Slot.svelte'
 </script>
 
 <Meta
@@ -72,6 +74,20 @@
 <Template let:args>
   <Collapse {...args} />
 </Template>
+
+<Story name="Slots" let:args>
+  <SlotInfo description="The collapse has some slots to allow customization">
+    <Slot name="default" explanation="The content of the collapse">
+      <Collapse {...args} title="Title">Collapse content</Collapse>
+    </Slot>
+    <Slot name="icon" explanation="The icon for the collapse. Displayed before the title">
+      <Collapse {...args} title="Title">
+        <Icon name="country-nz" slot="icon"/>
+        Collapse content
+      </Collapse>
+    </Slot>
+  </SlotInfo>
+</Story>
 
 <Story name="Primary" let:args>
   <div style={getStyleFromArgs(args)}>
