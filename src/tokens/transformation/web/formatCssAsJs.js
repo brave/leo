f272fc26bfb6aebd1f49263beafb3887c41a9aa7
Jay Harris <jay.harris@outlook.co.nz>
2023-06-29 12:46:19 +1200

    Fix JS tokens

--- src/tokens/transformation/web/formatCssAsJs.js
+++ src/tokens/transformation/web/formatCssAsJs.js
@@ -26,7 +26,13 @@ function removeSegmentFromNameInAllTokens(tokenCategory, nameSegment) {
       }
       continue
     }
-    result[key] = removeSegmentFromNameInAllTokens(value, nameSegment)
+
+    const cleaned = removeSegmentFromNameInAllTokens(value, nameSegment)
+    if (key === nameSegment) {
+      Object.assign(result, cleaned)
+    } else {
+      result[key] = cleaned
+    }
   }
   return result
 }
@@ -71,7 +77,12 @@ function formattedVariables(properties) {
 
 module.exports = ({ dictionary, file }) => {
   let fileContents = fileHeader({ file }) + '\n'
-  const themeObject = formattedVariables(dictionary.properties)
+
+  // The `desktop` segment is ignored in our property names, so make sure we
+  // remove it from the properties, or this won't match up with the actual CSS
+  // variables.
+  const properties = removeSegmentFromNameInAllTokens(dictionary.properties, 'desktop')
+  const themeObject = formattedVariables(properties)
   // Separate out each main property, to allow for tree shaking and easy type-to-complete
   // imports in code editors.
   for (const property in themeObject) {
