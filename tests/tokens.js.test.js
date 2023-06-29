e46392a942b58f30adef014ce12577ceaa05f75e
Jay Harris <jay.harris@outlook.co.nz>
2023-06-29 15:06:10 +1200

    Fix JS tokens (#307)

---
+++ tests/tokens.js.test.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+describe('JS tokens exist as CSS variables', () => {
+  const css = fs.readFileSync('./tokens/css/variables.css', 'utf-8')
+  const tokenExists = (token) => css.includes(` ${token}:`)
+
+  // Unfortunately because the variables JS is an ESModule we can't just
+  // import it here.
+  const variables = fs.readFileSync('./tokens/css/index.js', 'utf-8')
+  const tokenRegex = /var\((.*)\)/gi
+
+  const matches = variables.matchAll(tokenRegex)
+
+  for (const match of matches) {
+    it(`${match[1]} is a real CSS variable in variables.css`, () => {
+      expect(tokenExists(match[1])).toBe(true)
+    })
+  }
+})
