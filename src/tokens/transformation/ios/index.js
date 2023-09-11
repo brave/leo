7f2dfddb70aff1501ef5a2f3c640a8c74a7343ee
Kyle Hickinson <kylehickinson@users.noreply.github.com>
2023-09-11 16:15:37 -0400

    Add iOS color & gradient Swift generation (#380)

--- src/tokens/transformation/ios/index.js
+++ src/tokens/transformation/ios/index.js
@@ -1,3 +1,8 @@
+const StyleDictionary = require('style-dictionary')
+const _template = require('lodash/template')
+const fs = require('fs')
+const changeCase = require('change-case')
+
 module.exports = {
   transform: {},
   format: {},
@@ -6,3 +11,61 @@ module.exports = {
     'ios/fontStyles': require('./fontStyles')
   }
 }
+
+StyleDictionary.registerFormat({
+  name: 'ios/gradients',
+  formatter: ({ dictionary, options, file }) => {
+    const template = _template(
+      fs.readFileSync(__dirname + '/templates/Gradients.swift.template')
+    )
+
+    const gradients = {
+      linear: dictionary.allTokens
+        .filter(
+          (token) =>
+            token.type === 'custom-gradient' &&
+            token.value.gradientType === 'linear'
+        )
+        .map((token) => {
+          token.name = changeCase.camelCase(token.path.pop())
+          return token
+        })
+    }
+
+    return template({ gradients, options, file })
+  }
+})
+
+StyleDictionary.registerFormat({
+  name: 'ios/colorSetAccessors',
+  formatter: ({ dictionary, options, file }) => {
+    const template = _template(
+      fs.readFileSync(__dirname + '/templates/ColorSetAccessors.swift.template')
+    )
+
+    const colors = {}
+    dictionary.allTokens
+      .filter((token) => token.type === 'color')
+      .forEach((token) => {
+        if (token.name === 'ColorBlack' || token.name === 'ColorWhite') {
+          // The system already provides these and will conflict with the names
+          return
+        }
+        const name = changeCase.snakeCase(
+          token.path
+            .filter(
+              (path) => path !== 'dark' && path !== 'light' && path !== 'color'
+            )
+            .join(' ')
+        )
+        colors[name] = changeCase.camelCase(name).replace('_', '') // Xcode 15 accessors generate without underscores
+      })
+    return template({ colors, options, file })
+  }
+})
+
+StyleDictionary.registerAction({
+  name: 'ios/colorSets',
+  do: require('./colorsets').do,
+  undo: require('./colorsets').undo
+})
