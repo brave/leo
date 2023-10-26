e370e5d238ac5f056f1eb3f01a9d02062a6107c4
Jacob Lamont <cob@jacoblamont.com>
2023-10-25 21:34:00 -0400

    splits tailwind component plugins into individual imports

--- src/tokens/transformation/tailwind/static/plugins/index.ts
+++ src/tokens/transformation/tailwind/static/plugins/index.ts
@@ -1,10 +1,17 @@
-const { readdirSync } = require('fs')
-const { parse } = require('path')
+const plugins: string[] = []
 
-const plugins = readdirSync(__dirname).filter((file) => {
-  const { name } = parse(file)
-  return name !== 'index' && !name.startsWith('_')
-})
+// Prevent global issues with TS
+;(function () {
+  const { readdirSync } = require('fs')
+  const { parse } = require('path')
+
+  for (const file of readdirSync(__dirname, { withFileTypes: true })) {
+    const { name } = parse(file.name)
+    if (file.isFile() && name !== 'index' && !name.startsWith('_')) {
+      plugins.push(name)
+    }
+  }
+})()
 
 module.exports = plugins.map((file) => {
   return require(`./${file}`)
