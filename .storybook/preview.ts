cb4546bbd92e83c754e4e1035a78215745a61e12
Jacob Lamont <cob@jacoblamont.com>
2023-12-05 18:32:18 -0500

    [Tokens]: fixes font token stories display

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,10 +9,7 @@ import '@fontsource/manrope/400.css'
 import '@fontsource/manrope/500.css'
 import '@fontsource/manrope/600.css'
 import '@fontsource/manrope/700.css'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import '@fontsource/inter/600.css'
-import '@fontsource/inter/700.css'
+import '@fontsource-variable/inter'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
