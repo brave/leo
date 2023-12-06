71a16f830174ef5c7ba9813ddaabfcbc502a06b4
Jacob Lamont <jlamont@brave.com>
2023-12-06 13:06:43 -0500

    [SegmentedControl]: adds SegmentedControl component (#489)

---
+++ src/components/segmentedControl/segmentedControl.stories.svelte
@@ -0,0 +1,127 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+
+  import Slot from '../../storyHelpers/Slot.svelte'
+  import SlotInfo from '../../storyHelpers/SlotInfo.svelte'
+  import ControlItem from '../controlItem/controlItem.svelte'
+  import Icon from '../icon/icon.svelte'
+  import SegmentedControl, {
+    segmentedControlSizes
+  } from './segmentedControl.svelte'
+
+  let selected = 'full'
+</script>
+
+<Meta
+  title="Components/SegmentedControl"
+  component={SegmentedControl}
+  argTypes={{
+    '--leo-control-padding': {
+      control: 'text',
+      type: 'string',
+      description: 'The padding for the entire control.'
+    },
+    '--leo-control-item-radius': {
+      control: 'text',
+      type: 'string',
+      description: 'The radius for the individual control items.'
+    },
+    '--leo-control-item-height': {
+      control: 'text',
+      type: 'string',
+      description: 'The height for the individual control items.'
+    },
+    '--leo-control-item-padding': {
+      control: 'text',
+      type: 'string',
+      description: 'The padding for the individual control items.'
+    },
+    '--leo-control-item-color': {
+      control: 'text',
+      type: 'string',
+      description: 'The text color for the individual control items.'
+    },
+    '--leo-control-item-font': {
+      control: 'text',
+      type: 'string',
+      description:
+        'The font (shorthand CSS property) for the individual control items.'
+    },
+    '--leo-control-item-background': {
+      control: 'text',
+      type: 'string',
+      description: 'The background color for the individual control items.'
+    },
+    '--leo-control-item-shadow': {
+      control: 'text',
+      type: 'string',
+      description:
+        "The box shadow for the individual control items' hover state."
+    },
+    '--leo-control-item-icon-color': {
+      control: 'text',
+      type: 'string',
+      description: 'The icon color for the individual control items.'
+    },
+    '--leo-control-item-icon-gap': {
+      control: 'text',
+      type: 'string',
+      description:
+        'The gap between icon and content for the individual control items.'
+    },
+    size: {
+      control: 'select',
+      options: segmentedControlSizes
+    },
+    value: {
+      table: {
+        disable: true
+      }
+    },
+    segmentedControlSizes: {
+      table: {
+        disable: true
+      }
+    }
+  }}
+  args={{}}
+/>
+
+<Template let:args>
+  <p>Selected item: <code>{selected}</code></p>
+  {#key args.size}
+    <SegmentedControl {...args} bind:value={selected}>
+      <ControlItem value="full">
+        <Icon slot="icon-before" name="check-circle-outline" />
+        Full
+      </ControlItem>
+
+      <ControlItem value="simple">
+        <Icon slot="icon-before" name="check-circle-outline" />
+        Simple
+      </ControlItem>
+    </SegmentedControl>
+  {/key}
+</Template>
+
+<Story name="Slots">
+  <SlotInfo
+    description="This component accepts one slot. This slot should only contain ControlItem components."
+  >
+    <Slot name="default" explanation="The individual segments for the control.">
+      <SegmentedControl bind:value={selected}>
+        <ControlItem value="full">
+          <Icon slot="icon-before" name="check-circle-outline" />
+          Full
+        </ControlItem>
+
+        <ControlItem value="simple">
+          <Icon slot="icon-before" name="check-circle-outline" />
+          Simple
+        </ControlItem>
+      </SegmentedControl>
+    </Slot>
+  </SlotInfo>
+</Story>
+
+<Story name="Default" />
---
+++ src/components/segmentedControl/segmentedControl.svelte
@@ -0,0 +1,186 @@
+<script lang="ts" context="module">
+  export const segmentedControlSizes = ['default', 'small', 'tiny'] as const
+  type Size = (typeof segmentedControlSizes)[number]
+</script>
+
+<script lang="ts">
+  import { createEventDispatcher, onMount } from 'svelte'
+
+  export let value: string | undefined = undefined
+  export let size: Size = 'default'
+
+  let segmentedControl: HTMLDivElement
+
+  let pillWidth: number
+  let pillPosition: number
+
+  const dispatch = createEventDispatcher<{
+    'change': { value: string | undefined }
+  }>()
+
+  function getValue(e: Element) {
+    // If the option element doesn't have a value, fallback to using the text
+    // content - this allows writing simplified options:
+    // i.e. <o>1</o>
+    return e.getAttribute('value') ?? e['value'] ?? e.textContent
+  }
+
+  $: controlItems = Array.from(
+    (segmentedControl?.querySelector('slot') as HTMLSlotElement)
+      ?.assignedElements()
+      ?.filter((element) => element.tagName === 'LEO-CONTROLITEM') ??
+      segmentedControl?.querySelectorAll('.leo-control-item') ??
+      []
+  ) as HTMLElement[]
+
+  $: {
+    for (const controlItem of controlItems) {
+      controlItem.setAttribute('role', 'option')
+
+      if (value === getValue(controlItem)) {
+        controlItem.setAttribute('aria-selected', '')
+        window.requestAnimationFrame(() => {
+          pillWidth = controlItem.getBoundingClientRect().width
+          pillPosition = controlItem.offsetLeft
+        })
+      } else controlItem.removeAttribute('aria-selected')
+    }
+  }
+
+  function selectItem(e: Event) {
+    // Find the option which was clicked on, if any.
+    const item = controlItems.find((item) => e.composedPath().includes(item))
+
+    // If the event was triggered for something which isn't an option don't fire
+    // a change event.
+    if (!item) return
+
+    value = getValue(item)
+
+    dispatch('change', { value })
+  }
+
+  let pill: HTMLDivElement
+
+  onMount(() => {
+    pill.addEventListener('transitionstart', () => {
+      segmentedControl.classList.add('transitioning')
+    })
+
+    pill.addEventListener('transitionend', () => {
+      segmentedControl.classList.remove('transitioning')
+    })
+  })
+</script>
+
+<div
+  bind:this={segmentedControl}
+  class="leo-segmented-control size-{size}"
+  role="listbox"
+  tabindex="-1"
+  on:keypress={(e) => {
+    if (e.code !== 'Enter' && e.code !== 'Space') return
+    selectItem(e)
+  }}
+  on:click={selectItem}
+>
+  <div
+    class="pill"
+    style:width={`${pillWidth}px`}
+    style:left={`${pillPosition}px`}
+    bind:this={pill}
+  />
+  <slot />
+</div>
+
+<style lang="scss">
+  :host {
+    display: flex;
+    position: relative;
+  }
+
+  .leo-segmented-control {
+    --leo-icon-size: var(--leo-icon-m);
+    --bg: var(--leo-color-container-highlight);
+    --control-padding: var(--leo-control-padding, var(--leo-spacing-s));
+    --gap: var(--leo-spacing-s);
+    --control-height: 48px;
+
+    --leo-control-item-padding: var(--leo-spacing-xl);
+    --leo-control-item-icon-gap: var(--leo-spacing-m);
+    --leo-control-item-font: var(--leo-font-components-button-default);
+
+    display: flex;
+    max-width: max-content;
+    align-items: center;
+    position: relative;
+
+    background: var(--leo-segmented-control-bg, var(--bg));
+    padding: var(--control-padding);
+    gap: var(--leo-segmented-control-gap, var(--gap));
+    height: var(--leo-segmented-control-height, var(--control-height));
+    border-radius: var(--leo-segmented-control-height, var(--control-height));
+
+    &.size-small {
+      --leo-icon-size: var(--leo-icon-s);
+      --control-height: 40px;
+      --leo-control-item-padding: var(--leo-spacing-l);
+      --leo-control-item-font: var(--leo-font-components-button-small);
+    }
+
+    &.size-tiny {
+      --leo-icon-size: var(--leo-icon-xs);
+      --control-padding: var(--leo-control-padding, var(--leo-spacing-xs));
+      --gap: var(--leo-spacing-xs);
+      --control-height: 28px;
+      --leo-control-item-padding: var(--leo-spacing-m);
+      --leo-control-item-font: var(--leo-font-components-button-small);
+    }
+
+    .pill {
+      background: var(--leo-color-container-background);
+      position: absolute;
+      height: calc(100% - (var(--control-padding) * 2));
+      min-width: var(--control-height);
+      border-radius: calc(var(--control-height) - var(--control-padding));
+      transition:
+        width 0.2s cubic-bezier(0.22, 1, 0.36, 1),
+        left 0.4s cubic-bezier(0.22, 1, 0.36, 1);
+    }
+
+    :where(&) > :global .leo-control-item,
+    :where(&) > :global ::slotted(leo-controlitem) {
+      --leo-control-item-icon-color: var(--leo-color-icon-default);
+      --leo-control-item-color: var(--leo-color-text-secondary);
+      --leo-control-item-background: transparent;
+      --leo-control-item-radius: calc(
+        var(--control-height) - var(--control-padding)
+      );
+    }
+
+    :where(&:not(.transitioning)) > :global .leo-control-item:hover,
+    :where(&:not(.transitioning)) > :global ::slotted(leo-controlitem:hover) {
+      --leo-control-item-background: var(--leo-color-page-background);
+      --leo-control-item-color: var(--leo-color-text-primary);
+      --leo-control-item-shadow: var(--leo-effect-elevation-01);
+    }
+
+    :where(&) > :global .leo-control-item:focus-visible,
+    :where(&) > :global ::slotted(leo-controlitem:focus-visible) {
+      --leo-control-item-shadow: var(--leo-effect-focus-state);
+    }
+
+    :where(&) > :global .leo-control-item[aria-selected],
+    :where(&) > :global ::slotted(leo-controlitem[aria-selected]) {
+      --leo-control-item-color: var(--leo-color-text-primary);
+    }
+
+    :where(&.transitioning) > :global .leo-control-item[aria-selected],
+    :where(&.transitioning) > :global ::slotted(leo-controlitem[aria-selected]),
+    :where(&) > :global .leo-control-item[aria-selected]:hover,
+    :where(&) > :global ::slotted(leo-controlitem[aria-selected]:hover) {
+      --leo-control-item-icon-color: currentColor;
+      --leo-control-item-background: var(--leo-color-container-background);
+    }
+  }
+</style>
