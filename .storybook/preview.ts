ddae868b1a045b918b90648712a0457d10d715c2
Jacob Lamont <jlamont@brave.com>
2023-12-07 16:15:14 -0500

    [Storybook]: removes unused fonts from storybook config (#503)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,8 @@
 import Layout from './global.svelte'
 import { getNonStyleArgs, getStyleFromArgs } from './argHelper'
 
-import '@fontsource/poppins/400.css'
 import '@fontsource/poppins/500.css'
 import '@fontsource/poppins/600.css'
-import '@fontsource/poppins/700.css'
-import '@fontsource/manrope/400.css'
-import '@fontsource/manrope/500.css'
-import '@fontsource/manrope/600.css'
-import '@fontsource/manrope/700.css'
 import '@fontsource-variable/inter'
 
 export const parameters = {
