6121e47fbcd39ae293d9b7c66787ba93226d319a
deeppandya <deeppandya91@gmail.com>
2024-03-19 01:42:05 -0400

    Add gen-android-icons.js
    Update build command to include android icons

---
+++ src/scripts/gen-android-icons.js
@@ -0,0 +1,24 @@
+const fs = require('fs/promises')
+const path = require('path')
+const svg2vectordrawable = require('svg2vectordrawable')
+
+const ICONS_FOLDER = 'icons/'
+const OUTPUT_FOLDER = './tokens/android/drawable/'
+
+fs.mkdir(OUTPUT_FOLDER, { recursive: true })
+  .then(() => fs.readdir(ICONS_FOLDER))
+  .then((icons) => icons.filter((i) => i.endsWith('.svg')))
+  .then((icons) =>
+    icons.map(async (icon) => {
+      const svgContent = await fs.readFile(path.join(ICONS_FOLDER, icon), {
+        encoding: 'utf8'
+      })
+      svg2vectordrawable(svgContent).then((vectorContent) => {
+        const outputFileName = `ic_${path
+          .basename(icon, '.svg')
+          .replaceAll('-', '_')}.xml`
+        fs.writeFile(path.join(OUTPUT_FOLDER, outputFileName), vectorContent)
+        console.log(outputFileName)
+      })
+    })
+  )
--- src/tokens/transformation/android/filters/lightColor.js
+++ src/tokens/transformation/android/filters/lightColor.js
@@ -1,4 +1,5 @@
 const { matchLightThemeToken } = require('../../common/tokenFilters')
 
 module.exports = (token) =>
+  // We are adding primitive colors in light colors as values/colors.xml would be the default color file and we don't have dark version of primitive colors.
   matchLightThemeToken(token) || token.name.startsWith('primitive')
