1575f2faa1471262dbdc15f129fb834c86c48107
taher <8665427+nullhook@users.noreply.github.com>
2024-04-03 13:53:42 -0700

    button menu can be controlled (#617)

--- src/components/buttonMenu/buttonMenu.svelte
+++ src/components/buttonMenu/buttonMenu.svelte
@@ -15,24 +15,37 @@
 
 <script lang="ts">
   import Menu from '../menu/menu.svelte'
+  import { createEventDispatcher } from 'svelte'
 
-  export let isOpen = false
+  export let isOpen: boolean | undefined = undefined
+
   let anchor: HTMLElement
+
+  const dispatch = createEventDispatcher<{
+    change: { isOpen: boolean }
+  }>()
+
+  $: controlled = isOpen !== undefined
+
+  const toggle = () => {
+    if (!controlled) isOpen = !isOpen
+    dispatch('change', { isOpen })
+  }
 </script>
 
 <div class="leo-button-menu">
-  <!-- 
+  <!--
     We disable this check - the menu is triggered by the contained element
     firing a `click` event. This may be triggered with a keypress or click.
   -->
   <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div bind:this={anchor} on:click|stopPropagation={() => (isOpen = !isOpen)}>
+  <div bind:this={anchor} on:click|stopPropagation={toggle}>
     <slot name="anchor-content">Click</slot>
   </div>
   <Menu
     bind:isOpen
     target={anchor}
-    on:click={() => (isOpen = !isOpen)}
+    on:click={toggle}
     on:close
   >
     <slot />
