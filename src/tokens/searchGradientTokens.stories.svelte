21168c7eaa2bfa3df1152339ffae085b0d794e2e
Jacob Lamont <cob@jacoblamont.com>
2024-05-06 15:04:55 -0400

    [Tokens]: add search tokens

---
+++ src/tokens/searchGradientTokens.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Meta, Story } from '@storybook/addon-svelte-csf'
+  import { gradient as allGradients } from '../../tokens/css/variables-search'
+  import ColorTokenSwatchGroup from '../storyHelpers/ColorTokenSwatchGroup.svelte'
+  import '../../tokens/css/variables-search.css'
+</script>
+
+<Meta title="Tokens/Search/Gradients" />
+
+<Story name="All Gradients">
+  <ColorTokenSwatchGroup tokens={allGradients} swatchSize={300} />
+</Story>
