fd4b888c2191edace874cb4ae05aaff81f7ed47a
Agustín Ruiz <aruiz@brave.com>
2024-07-22 11:24:45 -0300

    fixed missing ;

--- src/components/menu/menu.svelte
+++ src/components/menu/menu.svelte
@@ -261,7 +261,7 @@
     cursor: pointer;
     margin: var(--leo-menu-item-margin);
     border-radius: var(--leo-menu-item-border-radius);
-    padding: var(--leo-menu-item-padding)
+    padding: var(--leo-menu-item-padding);
     display: revert;
   }
 
