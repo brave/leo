29151784e12e60b9b5a16cf478cad4f6b6ca17d1
brave-support-admin <138038132+brave-support-admin@users.noreply.github.com>
2024-07-31 15:50:06 +0100

    [Icons]: Update icons from Figma (#758)

--- icons/meta.d.ts
+++ icons/meta.d.ts
@@ -1,7 +1,7 @@
 import { StringWithAutoComplete } from '../src/types/string'
 
 const meta = {
-  "updatedAt": 1722240608219,
+  "updatedAt": 1722413400425,
   "icons": {
     "-pac-color": "-pac-color",
     "0xbtc-color": "0xbtc-color",
--- icons/meta.js
+++ icons/meta.js
@@ -1,5 +1,5 @@
 export default {
-  "updatedAt": 1722240608219,
+  "updatedAt": 1722413400425,
   "icons": {
     "-pac-color": "-pac-color",
     "0xbtc-color": "0xbtc-color",
