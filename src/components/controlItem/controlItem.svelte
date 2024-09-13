6219c2fc4dd37e464a6be407450269c77e2f6c1a
Jacob Lamont <jlamont@brave.com>
2024-09-13 12:32:22 -0400

    [SegmentedControl]: ensure controlItem doesn't shrink past its contents (#826)

--- src/components/controlItem/controlItem.svelte
+++ src/components/controlItem/controlItem.svelte
@@ -18,14 +18,14 @@
   :host {
     display: flex;
     height: 100%;
-    flex: 1 0 0;
+    flex: 1 0 auto;
   }
   .leo-control-item {
     all: unset;
     display: flex;
     align-items: center;
     justify-content: center;
-    flex: 1 0 0;
+    flex: 1 0 auto;
     box-sizing: border-box;
     height: 100%;
     border-radius: var(--leo-control-item-radius);
