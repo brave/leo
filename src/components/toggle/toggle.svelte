6115ba307054942f733f3b9551625a481e0f324f
Agustín Ruiz <aruiz@brave.com>
2024-11-27 15:37:00 -0300

    Revert "fix error on themed toggle"
    This reverts commit df8d4635e4f8be6fda46394eb55432125df50231.

--- src/components/toggle/toggle.svelte
+++ src/components/toggle/toggle.svelte
@@ -241,7 +241,7 @@
       .thumb {
         --thumb-offset: var(--checked-thumb-offset);
         color: var(--checked-color);
-        --thumb-disabled-color: var(--leo-toggle-thumb-disabled-color, white);
+        --thumb-disabled-color: var(--leo-color-schemes-on-primary);
 
         .on-icon {
           opacity: 1;
