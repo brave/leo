b385ea8c70153868c95094e20e8c69613ab98f21
Agustín Ruiz <aruiz@brave.com>
2025-06-26 18:26:14 -0300

    Required changes

--- src/components/tabs/tabs.svelte
+++ src/components/tabs/tabs.svelte
@@ -1,94 +1,179 @@
 <script lang="ts">
-  import { setContext } from 'svelte'
-  import { writable, type Writable } from 'svelte/store'
+  import { onMount } from 'svelte'
   import type { TabSize } from './props'
 
+  export let value: string | undefined = undefined
   export let size: TabSize = 'large'
 
-  const activeTab = writable(0)
-  const tabs = writable([])
-  const panels = writable([])
-  const tabElements = writable<HTMLElement[]>([])
+  export let onChange: (detail: { value: string | undefined }) => void =
+    undefined
 
-  setContext('activeTab', activeTab)
-  setContext('tabs', {
-    subscribe: tabs.subscribe,
-    add: (tab: Writable<any>) => {
-      let index
-      tabs.update(items => {
-        index = items.length
-        if (items.length === 0) {
-          activeTab.set(0)
-        }
-        return [...items, tab]
-      })
-      return index
-    },
-    registerElement: (index: number, element: HTMLElement) => {
-      tabElements.update(elements => {
-        elements[index] = element
-        return elements
-      })
-    },
-  })
-  setContext('panels', {
-    subscribe: panels.subscribe,
-    add: (panel: Writable<any>) => {
-      let index
-      panels.update(items => {
-        index = items.length
-        return [...items, panel]
-      })
-      return index
-    },
-  })
-  setContext('size', size)
+  let tabsContainer: HTMLDivElement
 
-  let underlineStyle = ''
-  $: if ($tabElements[$activeTab]) {
-    const element = $tabElements[$activeTab]
-    underlineStyle = `left: ${element.offsetLeft}px; width: ${element.offsetWidth}px;`
+  let underlineWidth: number
+  let underlinePosition: number
+
+  function getValue(e: Element) {
+    // If the tab element doesn't have a value, fallback to using the text
+    // content - this allows writing simplified tabs:
+    // i.e. <TabItem>Home</TabItem>
+    return e.getAttribute('value') ?? e['value'] ?? e.textContent
   }
-</script>
 
-<div class="tabs-container" class:medium={size === 'medium'} class:small={size === 'small'}>
-  <div class="tabs" role="tablist">
-    <slot name="tabs" />
-    <div class="underline" style={underlineStyle} />
-  </div>
-  <div class="tab-panels">
-    <slot name="tab-panels" />
-  </div>
-</div>
+  $: tabItems = Array.from(
+    (tabsContainer?.querySelector('slot') as HTMLSlotElement)
+      ?.assignedElements()
+      ?.filter((element) => element.tagName === 'LEO-TABITEM') ??
+      tabsContainer?.querySelectorAll('.leo-tab-item') ??
+      []
+  ) as HTMLElement[]
 
-<style lang="scss">
-  .tabs-container {
-    --indicator-color: var(--leo-color-text-interactive);
-    --indicator-height: 4px;
+  function setUnderline(item: HTMLElement) {
+    underlineWidth = item.getBoundingClientRect().width
+    underlinePosition = item.offsetLeft
+  }
 
-    &.medium,
-    &.small {
-      --indicator-height: 3px;
+  const itemResizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      setUnderline(entry.target as HTMLElement)
+    }
+  })
+
+  $: {
+    for (const tabItem of tabItems) {
+      tabItem.setAttribute('role', 'tab')
+
+      if (value === getValue(tabItem)) {
+        tabItem.setAttribute('aria-selected', 'true')
+        itemResizeObserver.observe(tabItem)
+      } else {
+        tabItem.setAttribute('aria-selected', 'false')
+        itemResizeObserver.unobserve(tabItem)
+      }
     }
   }
 
-  .tabs {
+  function selectItem(e: Event) {
+    // Find the tab which was clicked on, if any.
+    const item = tabItems.find((item) => e.composedPath().includes(item))
+
+    // If the event was triggered for something which isn't a tab don't fire
+    // a change event.
+    if (!item) return
+
+    value = getValue(item)
+
+    onChange?.({ value })
+  }
+
+  let underline: HTMLDivElement
+
+  onMount(() => {
+    underline.addEventListener('transitionstart', () => {
+      tabsContainer.classList.add('transitioning')
+    })
+
+    underline.addEventListener('transitionend', () => {
+      tabsContainer.classList.remove('transitioning')
+    })
+  })
+</script>
+
+<div
+  bind:this={tabsContainer}
+  class="leo-tabs size-{size}"
+  role="tablist"
+  tabindex="-1"
+  on:keypress={(e) => {
+    if (e.code !== 'Enter' && e.code !== 'Space') return
+    selectItem(e)
+  }}
+  on:click={selectItem}
+>
+  <div
+    class="underline"
+    style:width={`${underlineWidth}px`}
+    style:left={`${underlinePosition}px`}
+    bind:this={underline}
+  />
+  <slot />
+</div>
+
+<style lang="scss">
+  :host {
+    display: flex;
+    position: relative;
+    width: var(--leo-tabs-width, 100%);
+
+    & .leo-tabs {
+        width: 100%;
+    }
+  }
+
+  .leo-tabs {
+    --leo-icon-size: var(--leo-icon-size-m);
+    --indicator-color: var(--leo-color-text-interactive);
+    --indicator-height: 4px;
+
+    --leo-tab-item-padding: 20px var(--leo-spacing-l);
+    --leo-tab-item-icon-gap: var(--leo-spacing-m);
+    --leo-tab-item-font: var(--leo-font-components-button-large);
+
+    display: flex;
     position: relative;
     border-bottom: 1px solid var(--leo-color-divider-subtle);
-  }
+    overflow-x: auto;
 
-  .tabs > :global(div) {
-      display: flex;
-      flex-wrap: nowrap;
-      overflow-x: auto;
-  }
+    &.size-medium {
+      --indicator-height: 3px;
+      --leo-tab-item-padding: var(--leo-spacing-xl) var(--leo-spacing-l);
+      --leo-tab-item-font: var(--leo-font-components-button-default);
+    }
 
-  .underline {
-    position: absolute;
-    bottom: -1px;
-    height: var(--indicator-height);
-    background-color: var(--indicator-color);
-    transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
-    border-radius: 3px 3px 0 0;
+    &.size-small {
+      --leo-icon-size: var(--leo-icon-size-s);
+      --indicator-height: 3px;
+      --leo-tab-item-padding: var(--leo-spacing-l) var(--leo-spacing-m);
+      --leo-tab-item-font: var(--leo-font-components-button-small);
+    }
+
+    .underline {
+      position: absolute;
+      bottom: -1px;
+      height: var(--indicator-height);
+      background-color: var(--indicator-color);
+      transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+      border-radius: 3px 3px 0 0;
+    }
+
+    :where(&) > :global(.leo-tab-item),
+    :where(&) > :global(::slotted(leo-tabitem)) {
+      --leo-tab-item-icon-color: var(--leo-color-icon-default);
+      --leo-tab-item-color: var(--leo-color-text-secondary);
+      --leo-tab-item-background: transparent;
+    }
+
+    :where(&:not(.transitioning)) > :global(.leo-tab-item:hover),
+    :where(&:not(.transitioning)) > :global(::slotted(leo-tabitem:hover)) {
+      --leo-tab-item-color: var(--leo-color-text-interactive);
+      --leo-tab-item-icon-color: var(--leo-color-icon-interactive);
+    }
+
+    :where(&) > :global(.leo-tab-item:focus-visible),
+    :where(&) > :global(::slotted(leo-tabitem:focus-visible)) {
+      --leo-tab-item-shadow: var(--leo-effect-focus-state);
+    }
+
+    :where(&) > :global(.leo-tab-item[aria-selected="true"]),
+    :where(&) > :global(::slotted(leo-tabitem[aria-selected="true"])) {
+      --leo-tab-item-color: var(--leo-color-text-interactive);
+      --leo-tab-item-icon-color: var(--leo-color-icon-interactive);
+    }
+
+    :where(&) > :global(.leo-tab-item[aria-selected="true"]:hover),
+    :where(&) > :global(::slotted(leo-tabitem[aria-selected="true"]:hover)) {
+      --leo-tab-item-color: var(--leo-color-blurple-60);
+      --leo-tab-item-icon-color: var(--leo-color-blurple-60);
+    }
   }
 </style> 
