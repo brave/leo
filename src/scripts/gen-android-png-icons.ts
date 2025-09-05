0b109ced3ec06a8f252b987c80ea40e38ff80948
AlexeyBarabash <alexey@brave.com>
2025-09-05 16:21:40 +0300

    Added a command android-png-icons to generate android drawable set
    Usage: npm run android-png-icons -- <icon_name.svg> [new_color]

---
+++ src/scripts/gen-android-png-icons.ts
@@ -0,0 +1,53 @@
+import fs from 'fs/promises'
+import path from 'path'
+import sharp from 'sharp'
+
+const ICONS_FOLDER = 'icons/'
+
+async function svgToAndroidDrawablePngs(svgFileName, newColor) {
+  const densities = {
+    mdpi: 24,
+    hdpi: 36,
+    xhdpi: 48,
+    xxhdpi: 72,
+    xxxhdpi: 96
+  }
+
+  for (const [bucket, size] of Object.entries(densities)) {
+    await fs.mkdir(`./tokens/android/drawable-${bucket}/`, { recursive: true })
+
+    const outputFileName = svgFileName.replaceAll('-', '_') + '.png'
+
+    let svgContent = await fs.readFile(
+      path.join(ICONS_FOLDER, svgFileName + '.svg'),
+      {
+        encoding: 'utf8'
+      }
+    )
+
+    if (newColor) {
+      svgContent = svgContent.replace(
+        /fill="#[0-9A-Fa-f]{6}"/,
+        `fill="#${newColor}"`
+      )
+    }
+
+    await sharp(Buffer.from(svgContent, 'utf-8'))
+      .resize(size, size)
+      .png()
+      .toFile(`./tokens/android/drawable-${bucket}/${outputFileName}`)
+  }
+}
+
+if (process.argv.length < 3) {
+  console.error(
+    'Usage: npm run android-png-icons -- <icon_name.svg> [new_color]'
+  )
+} else {
+  let svgFileName = process.argv[2]
+  if (svgFileName.endsWith('.svg')) {
+    svgFileName = svgFileName.substring(0, svgFileName.length - 4)
+  }
+  const newColor = process.argv.length >= 3 ? process.argv[3] : undefined
+  await svgToAndroidDrawablePngs(svgFileName, newColor)
+}
