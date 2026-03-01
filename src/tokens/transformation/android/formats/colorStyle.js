216f7a3705e36289ed94618f8364a970066d8bfa
Jay Harris <jay.harris@outlook.co.nz>
2026-03-02 09:14:58 +1300

    [android]: Allow transparency in color tokens

--- src/tokens/transformation/android/formats/colorStyle.js
+++ src/tokens/transformation/android/formats/colorStyle.js
@@ -2,13 +2,19 @@ import StyleDictionary from 'style-dictionary'
 import { TinyColor } from '@ctrl/tinycolor'
 const { fileHeader } = StyleDictionary.formatHelpers
 
+const getTokenValue = (token) => {
+  const color = new TinyColor(token.original.value)
+  if (color.getAlpha() === 1) {
+    return color.toHexString()
+  }
+  return color.toHex8String()
+}
+
 export default ({ dictionary, platform, options = {}, file }) => {
   const colorStyles = dictionary.allTokens
     .filter((compositeToken) => compositeToken.type === 'color')
     .map((compositeToken) => {
-      const colorCode = new TinyColor(
-        compositeToken.original.value
-      ).toHexString()
+      const colorCode = getTokenValue(compositeToken)
       return `  <color name="${compositeToken.name}" tools:ignore="UnusedResources">${colorCode}</color>`
     })
   return `<?xml version="1.0" encoding="utf-8" ?>
