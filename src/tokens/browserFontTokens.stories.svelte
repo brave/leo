7b7ce5f67e4e9beff63621bc40f0d092bb47f149
Agustín Ruiz <aguscr182@gmail.com>
2026-06-03 18:31:52 -0300

    Refactor marketing font tokens and stories
    Rework marketing typography tokens and related Storybook entries. marketing.tokens.json was reorganized (new grouped categories like 🪩 marketing → display-primary/secondary, heading, default/large/small), switched primary fonts to Inter Variable (and Flecha for some display styles), adjusted metrics (letterSpacing, lineHeight, paragraphSpacing) and updated Figma style IDs. Added marketing.variables.json and a new searchColorTokens.stories.svelte; simplified browserFontTokens.stories.svelte to only show the "All Fonts" story. Other token story files were updated to align with the new token shape. This cleans up token structure and standardizes variable-font usage across marketing styles.

--- src/tokens/browserFontTokens.stories.svelte
+++ src/tokens/browserFontTokens.stories.svelte
@@ -16,19 +16,3 @@
 <Story name="All Fonts">
   <FontTokenSwatchGroup tokens={allFonts} />
 </Story>
-
-<Story name="Large">
-  <FontTokenSwatchGroup tokens={allFonts.large} />
-</Story>
-
-<Story name="Default">
-  <FontTokenSwatchGroup tokens={allFonts.default} />
-</Story>
-
-<Story name="Small">
-  <FontTokenSwatchGroup tokens={allFonts.small} />
-</Story>
-
-<Story name="Extra Small">
-  <FontTokenSwatchGroup tokens={allFonts.xSmall} />
-</Story>
---
+++ src/tokens/searchColorTokens.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  export const meta = {
+    title: 'Tokens/Search/Colors'
+  }
+</script>
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { Story } from '@storybook/addon-svelte-csf'
+  import { color as allColors } from '../../tokens/css/variables-search'
+  import ColorTokenSwatchGroup from '../storyHelpers/ColorTokenSwatchGroup.svelte'
+  import { scopedApplyLayer } from '../../.storybook/global.svelte'
+
+  onMount(() => scopedApplyLayer("variables-search"))
+
+  const { search } = allColors
+</script>
+
+<Story name="All Colors">
+  <ColorTokenSwatchGroup tokens={search} />
+</Story>
